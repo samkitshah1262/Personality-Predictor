2262cbfdf80aa574f325798d8fd56e6e02360fa3
samkitshah1262 <samkitshah1262@gmail.com>
2022-05-01 20:43:36 +0530

    transformer added

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import string
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 app = Flask(__name__)
-# model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 
 @app.route('/')
@@ -20,25 +20,17 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    # int_features = [int(x) for x in request.form.values()]
-    # final_features = [np.array(int_features)]
-    # prediction = model.predict(final_features)
+
     text=request.form.values()
     regex = re.compile('[%s]' % re.escape('|'))
-    # text = BeautifulSoup(text, "lxml").text
-    # text = re.sub(r'\|\|\|', r' ', text) 
-    # text = re.sub(r'http\S+', r'<URL>', text)
-    # text = regex.sub(" ", text)
     words = str(text).split()
     words = [i.lower() + " " for i in words]
     words = [i for i in words if not "http" in i]
     words = " ".join(words)
     words = words.translate(words.maketrans('', '', string.punctuation))
-    # output = round(prediction[0], 2)
     vectorizer=TfidfVectorizer(max_features=5000,stop_words='english',tokenizer=Lemmatizer())
-    # raw=vectorizer.fit(words).toarray()
-    # output=model.predict(raw)
-    output='success'
+    raw=vectorizer.fit(words).toarray()
+    output=model.predict(raw)
     return render_template('index.html', prediction_text='Your Personality Type is : {}'.format(output))
 
 # @app.route('/predict_api',methods=['POST'])
